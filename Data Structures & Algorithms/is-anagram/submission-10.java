904541dfdc557f8d28bb08f22bfc5b8926f88a11
Hritvik Soni <159177170+real-OP@users.noreply.github.com>
2026-03-30 22:15:23 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())return false;
+        HashMap <Character, Integer> map1 = new HashMap <>();
+        HashMap <Character, Integer> map2 = new HashMap <>();
+
+
+        for(char c : s.toCharArray()){
+            map1.put(c,map1.getOrDefault(c ,0) + 1);
+        }
+        for(char d : t.toCharArray()){
+            map2.put(d,map2.getOrDefault(d ,0) + 1);
+        }
+
+        for(char e : map1.keySet()){
+            if(!map1.get(e).equals(map2.get(e))){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+}
