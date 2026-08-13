dd324e91293a673b2f3128cb27f642abc928cb68
Hritvik Soni <159177170+real-OP@users.noreply.github.com>
2026-08-13 15:38:07 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet set = new HashSet<>();
+        for(int num : nums)set.add(num);
+        if(set.size() < nums.length){return true;}
+        return false;
+
+        
+    }
+}
