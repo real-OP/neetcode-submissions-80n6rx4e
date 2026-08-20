2a92ca4b7f9befceedcf0d4e088d7b10ea731a31
Hritvik Soni <159177170+real-OP@users.noreply.github.com>
2026-08-20 14:54:53 +0530

    Add: merge-strings-alternately - submission-3

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        if(word1 == null || word2 == null )return "";
+
+        StringBuilder result = new StringBuilder();
+
+        int s=0,p=0;
+
+        while(s < word1.length() || p < word2.length()){
+            if(s < word1.length()){result.append(word1.charAt(s++));}
+            if(p < word2.length()){result.append(word2.charAt(p++));}
+        }
+        return result.toString();
+
+        
+
+
+        
+    }
+}
