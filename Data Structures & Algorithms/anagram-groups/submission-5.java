8c6338296a37e66a242182d05f8031bd6197c180
Hritvik Soni <159177170+real-OP@users.noreply.github.com>
2026-08-28 19:37:49 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap <String, List <String>> map = new HashMap<>(); 
+        for(String str : strs){
+            char[] word = str.toCharArray();
+            Arrays.sort(word);
+            String key = new String(word);
+            map.putIfAbsent(key , new ArrayList<>());
+            map.get(key).add(str);
+            
+        }
+
+        List<List<String>> result = new ArrayList<>();
+
+        for(String str : map.keySet()){
+            result.add(map.get(str));
+        }
+        return result;
+
+        
+        
+        
+    }
+}
